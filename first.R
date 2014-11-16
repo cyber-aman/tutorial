1be3456746e3f7643172894eacfea46f9f6f7423
cyber-aman <cyber_aman@yahoo.com>
2014-11-16 11:59:42 +0530

    first R file fro Project tutorial

---
+++ first.R
@@ -0,0 +1,8 @@
+#Lets create a data frame
+weight = c(150, 135, 210, 140)
+height = c(65, 61, 70, 65)
+gender = c("Fe","Fe","M","Fe") 
+study = data.frame(weight,height,gender) # make the data frame
+study
+study[[1]]
+study[1]
